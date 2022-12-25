71b17385bc1702d82420f077a0c7443b12f410fc
Виталий <vitamin.vm0@gmail.com>
2022-12-25 10:12:55 +0300

    Декорирует элементы 1.2

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,7 @@
 let mainHeader = document.querySelector(".main-header");
+let headerToggle = document.querySelector(".main-header__toggle");
 mainHeader.classList.remove("main-header--closed");
+mainHeader.classList.remove("main-header--nojs");
 
 headerToggle.addEventListener("click", function () {
   if (mainHeader.classList.contains("main-header--closed")) {
