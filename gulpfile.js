a8890bd161c71604d2e9033134db525b74d47419
Виталий <vitamin.vm0@gmail.com>
2023-01-22 17:49:29 +0300

    Автоматизация 1.4

--- gulpfile.js
+++ gulpfile.js
@@ -138,7 +138,7 @@ const watcher = () => {
 
 //Build
 
-/* export const build = gulp.series (
+export const build = gulp.series (
   clean,
   copy,
   opimizeImages,
@@ -150,7 +150,7 @@ const watcher = () => {
     sprite,
     createWebp
   ),
-); */
+);
 
 //default
 
